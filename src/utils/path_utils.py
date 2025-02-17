895c6da56e2d0cb376d2ac22a7165ddc87b19e4c
joaodinizaraujo <joaovictorclassroom@gmail.com>
2025-02-17 16:53:31 -0300

    tirando função nao utilizada

--- src/utils/path_utils.py
+++ src/utils/path_utils.py
@@ -15,23 +15,3 @@ def create_directory_if_not_exists(*dirs: str) -> None:
     for directory in dirs:
         if not os.path.exists(directory):
             os.makedirs(directory)
-
-
-def join_without_overwriting(base_dir: str, file_name: str) -> str:
-    """
-    Gera um caminho único para o arquivo, evitando sobrescrita.
-    :param base_dir: diretório onde o arquivo será salvo
-    :param file_name: nome do arquivo a ser salvo
-    :return: caminho do arquivo com nome único
-    """
-
-    base_name, extension = os.path.splitext(file_name)
-    counter = 1
-    path = os.path.join(base_dir, file_name)
-
-    while os.path.exists(path):
-        new_file = f"{base_name}_{counter}{extension}"
-        path = os.path.join(base_dir, new_file)
-        counter += 1
-
-    return path
